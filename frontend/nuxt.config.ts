095f8f1a53c046cbb7f233ab88ee3ad4d355d856
winnie chang <z976200@gmail.com>
2024-11-22 17:54:02 +0800

    連接後端

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ['@nuxt/eslint', '@nuxtjs/strapi'],
+
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    version: 'v4',
+  },
+
+  runtimeConfig: {
+    strapi: {
+      url: process.env.STRAPI_URL,
+    },
+    public: {
+      strapi: {
+        url: process.env.STRAPI_URL,
+      },
+    },
+  },
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,23 @@
+<script setup lang="ts">
+const { find, findOne, create } = useStrapi()
+
+// GET
+const { data } = await find('vocabularies')
+// const aaa = await findOne('vocabularies', '5')
+
+// POST
+const onSubmit = async () => {
+  await create('vocabularies', {
+    name_jp: 'My restaurant2',
+    translate_ch: '1232',
+  })
+}
+</script>
+
 <template>
   <div>
     <div>q</div>
-    123
+    {{ data }}
+    <button @click="onSubmit">click</button>
   </div>
 </template>
