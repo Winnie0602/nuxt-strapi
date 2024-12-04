c83462347e1a2bb611be56dbae504fbb7de6d3df
winnie chang <z976200@gmail.com>
2024-12-04 16:09:45 +0800

    upd api data type

--- frontend/server/api/vocabularies.get.ts
+++ frontend/server/api/vocabularies.get.ts
@@ -1,15 +1,19 @@
 import { getServerSession } from '#auth'
+import type { Vocabulary } from '~/types/type'
 
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
 
   const config = useRuntimeConfig()
   if (session) {
-    return await $fetch(`${config.strapiBaseUrl}/vocabularies`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${session.user.jwt}`,
+    return await $fetch<{ data: Vocabulary[] }>(
+      `${config.strapiBaseUrl}/vocabularies`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${session.user.jwt}`,
+        },
       },
-    })
+    )
   }
 })
--- frontend/server/api/vocabulary.ts
+++ frontend/server/api/vocabulary.ts
@@ -1,4 +1,5 @@
 import { getServerSession } from '#auth'
+import type { Vocabulary } from '~/types/type'
 
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
@@ -8,7 +9,7 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
   if (session) {
-    const result = await $fetch(
+    const result = await $fetch<{ data: Vocabulary }>(
       `${config.strapiBaseUrl}/vocabularies/${body.documentId}`,
       {
         method: 'GET',
