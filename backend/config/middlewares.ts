01edf21eff7eb4cb10186a9122f2a1f502840989
winnie chang <z976200@gmail.com>
2025-02-10 16:29:19 +0800

    upd

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -6,12 +6,13 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
       origin: [
         'http://localhost:3000', // 本地开发时允许的前端请求地址
         'https://nuxt-strapi-7ztr.onrender.com', // 你部署在 Render 上的 Strapi 地址
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE'], // 允许的 HTTP 请求方法
+      allowedHeaders: ['Content-Type', 'Authorization'], // 允许的头部信息
+      credentials: true, // 允许发送凭证（比如 cookies）
     },
   },
   'strapi::poweredBy',
