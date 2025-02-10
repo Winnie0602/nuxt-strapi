7da42fdfe26b59d4932458f62a6e2dd78f8bef1f
winnie chang <z976200@gmail.com>
2025-02-10 13:49:04 +0800

    資料庫嘗試更換成postgres，因sqlite在docker持續發生錯誤

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  auth: {
+    baseURL: `http://localhost:${process.env.PORT || 3000}`
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
