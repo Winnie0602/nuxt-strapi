05dc0a91b29a871734bb713ec6e23eab089c3270
winnie chang <z976200@gmail.com>
2025-03-17 15:52:52 +0800

    變更web api speech語言為goole的voice

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -144,7 +144,11 @@ export default {
 
       const unicodeRegex =
         /[\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2300}-\u{23FF}]+$/gu
+
       const symbols = message.match(unicodeRegex)?.[0] || '' // 取出表情符號 不翻譯
+
+      if (symbols === message) return message
+
       const filteredMessage = message.replace(unicodeRegex, '') // 移除符號
 
       const response = await fetch(url, {
@@ -156,6 +160,7 @@ export default {
       if (!response.ok) return message // 翻譯失敗則返回原訊息
 
       const data = (await response.json()) as TranslateResponse
+
       return (data.data?.translations?.[0]?.translatedText || message).replace(/&#39;/g, "'") + symbols
     }
 
--- frontend/utils/speak.ts
+++ frontend/utils/speak.ts
@@ -5,12 +5,65 @@ export const speak = (
 ) => {
   const utterThis = new SpeechSynthesisUtterance(word)
 
-  utterThis.lang = lang
-  utterThis.rate = 0.8
+  const voiceMap = new Map<string, string>([
+    ['en-US', 'Google US English'],
+    ['zh-TW', 'Google 國語（臺灣）'],
+    ['zh-CN', 'Google 普通话（中国大陆）'],
+    ['zh-HK', 'Google 粤語（香港）'],
+    ['ja-JP', 'Google 日本語'],
+    ['ko-KR', 'Google 한국의'],
+    ['th-TH', 'Google हिन्दी'],
+    ['fr-FR', 'Google français'],
+  ])
 
-  window.speechSynthesis.speak(utterThis)
+  const voiceName = voiceMap.get(lang)
 
-  utterThis.onend = () => {
-    if (onEnd) onEnd() // 播放結束時執行回調函數
+  // 確保語音列表加載完成
+  const getVoices = () => {
+    return new Promise<SpeechSynthesisVoice[]>((resolve) => {
+      const voices = speechSynthesis.getVoices()
+
+      console.log(voices.map((e) => e.name))
+
+      if (voices.length) {
+        resolve(voices)
+      } else {
+        // 如果語音還未加載，等候加載完成
+        speechSynthesis.onvoiceschanged = () => {
+          resolve(speechSynthesis.getVoices())
+        }
+      }
+    })
   }
+
+  // 設定語音並播放
+  const setVoice = async () => {
+    const voices = await getVoices() // 等待語音列表加載完成
+
+    const selectedVoice = voices.find((voice) => voice.name === voiceName)
+
+    if (selectedVoice) {
+      utterThis.voice = selectedVoice
+    } else {
+      console.warn(`未找到名為 "${voiceName}" 的語音，使用預設語音`)
+
+      // 如果沒有找到，選擇第一個符合語言的語音
+      const defaultVoice = voices.find((voice) => voice.lang === lang)
+      if (defaultVoice) utterThis.voice = defaultVoice
+    }
+
+    utterThis.lang = lang
+    utterThis.rate = 1
+    utterThis.pitch = 1 // 可以根據需要調整音高
+
+    // 開始語音播放
+    window.speechSynthesis.speak(utterThis)
+
+    // 播放結束時的回調
+    utterThis.onend = () => {
+      if (onEnd) onEnd() // 播放結束時執行回調函數
+    }
+  }
+
+  setVoice() // 執行語音設置和播放
 }
